b46e091264f74bdb3d1fe198117fdc6cc4e334f1
Bissou1947 <bassil_1947@hotmail.com>
2023-03-13 20:12:19 +0200

    add mvc to app

---
+++ BookStoreWeb/BookStoreWeb/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreWeb.Controllers
+{
+    public class HomeController: Controller
+    {
+        public string Index()
+        {
+            return "Home";
+        }
+    }
+}
